e4b4b906480df3c6b3a6bbe1add47d3c4bd8e936
elyse <elyseewong@gmail.com>
2026-04-16 20:02:37 -0700

    Light/dark mode + Contact Form functionality

--- global.js
+++ global.js
@@ -29,9 +29,54 @@ for (let p of pages) {
   nav.append(a);
   if (a.host === location.host && a.pathname === location.pathname) {
     a.classList.add('current');
-} else {
-    a.target = '_blank';
+  }
+  a.host !== location.host && a.setAttribute('target', '_blank');
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme: 
+        <select>
+			<option value="light dark">Automatic${matchMedia("(prefers-color-scheme: dark)").matches ? " (Dark)" : " (Light)"}</option>
+			<option value="light">Light</option>
+			<option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+  select.value = colorScheme;
+  localStorage.colorScheme = colorScheme;
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
 }
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  setColorScheme(event.target.value);
+});
+
+const form = document.querySelector('form[action^="mailto:"]');
+form?.addEventListener('submit', function (event) {
+  event.preventDefault();
+  
+  const data = new FormData(form);
+  let url = form.action + '?';
+  
+  const params = [];
+  for (let [name, value] of data) {
+    params.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+  }
+  
+  url += params.join('&');
+  location.href = url;
+});
 
 
